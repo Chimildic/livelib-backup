a585239e8cf1ac0ecd6f005dc3c1cbe2a7511207
Chimildic <40138097+Chimildic@users.noreply.github.com>
2024-07-28 09:12:20 +0300

    Refactor: 4 spaces

--- src/gs/index.js
+++ src/gs/index.js
@@ -1,33 +1,33 @@
 function doGet() {
-  return HtmlService.createTemplateFromFile('page')
-    .evaluate()
-    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
-    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+    return HtmlService.createTemplateFromFile('page')
+        .evaluate()
+        .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
 }
 
 function include(filename) {
-  return HtmlService.createHtmlOutputFromFile(filename).getContent()
+    return HtmlService.createHtmlOutputFromFile(filename).getContent()
 }
 
 function getBiglist(args) {
-  let result
-  let response = LivelibClient.getBiglist(args)
-  if (response.error_code == 0) {
-    result = {
-      isLastPage: response.last_page,
-      pageNumber: response.page_no,
-      bookArray: BookParser.parseToBookArray(response.content, args.includeColumns)
-    }
-  } else {
-    result = {
-      isError: true,
-      code: response.error_code
-    }
-    if (result.code == -3) {
-      result.text = 'Пользователь не найден. Проверьте правильность имени.'
+    let result
+    let response = LivelibClient.getBiglist(args)
+    if (response.error_code == 0) {
+        result = {
+            isLastPage: response.last_page,
+            pageNumber: response.page_no,
+            bookArray: BookParser.parseToBookArray(response.content, args.includeColumns)
+        }
     } else {
-      result.text = `Livelib вернул ошибку c кодом ${result.code}`
+        result = {
+            isError: true,
+            code: response.error_code
+        }
+        if (result.code == -3) {
+            result.text = 'Пользователь не найден. Проверьте правильность имени.'
+        } else {
+            result.text = `Livelib вернул ошибку c кодом ${result.code}`
+        }
     }
-  }
-  return result
+    return result
 }
