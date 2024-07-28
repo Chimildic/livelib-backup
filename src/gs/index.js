e17a265e364956184fc05fcc0a3c8c6129c7a8c8
Chimildic <40138097+Chimildic@users.noreply.github.com>
2024-07-28 09:00:54 +0300

    Add: show modal when error or empty book array

--- src/gs/index.js
+++ src/gs/index.js
@@ -2,6 +2,7 @@ function doGet() {
   return HtmlService.createTemplateFromFile('page')
     .evaluate()
     .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
 }
 
 function include(filename) {
@@ -9,10 +10,24 @@ function include(filename) {
 }
 
 function getBiglist(args) {
+  let result
   let response = LivelibClient.getBiglist(args)
-  return {
-    isLastPage: response.last_page,
-    pageNumber: response.page_no,
-    bookArray: BookParser.parseToBookArray(response.content, args.includeColumns)
+  if (response.error_code == 0) {
+    result = {
+      isLastPage: response.last_page,
+      pageNumber: response.page_no,
+      bookArray: BookParser.parseToBookArray(response.content, args.includeColumns)
+    }
+  } else {
+    result = {
+      isError: true,
+      code: response.error_code
+    }
+    if (result.code == -3) {
+      result.text = 'Пользователь не найден. Проверьте правильность имени.'
+    } else {
+      result.text = `Livelib вернул ошибку c кодом ${result.code}`
+    }
   }
+  return result
 }
